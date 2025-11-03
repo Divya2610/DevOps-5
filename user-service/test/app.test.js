8db93df27bf7b71d170fdda2c9b9ba9a55c122bc
Divya <divyakhanolkar26@gmail.com>
2025-11-04 03:28:55 +0530

    initial commit

---
+++ user-service/test/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('../index')
+
+describe('GET /', () => {
+    it('should return User Service is running', async () => {
+        const res = await request(app).get('/')
+        expect(res.statusCode).toBe(200)
+        expect(res.text).toBe('User Service is running')
+    })
+})
+
+afterAll(() => {
+    server.close();
+});
